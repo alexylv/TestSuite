80f3ba49f8a70e1158fd719e39d91fd45a5fedc6
alexylv <alex.yliao71@gmail.com>
2024-12-27 00:29:01 -0500

    Add files via upload

---
+++ src/test/java/com/revature/steps/Hooks.java
@@ -0,0 +1,20 @@
+package com.revature.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import com.revature.TestRunner; 
+
+public class Hooks {
+
+    @After
+    public void takeScreenshotOnFailure(Scenario scenario) {
+  
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) TestRunner.driver).getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", scenario.getName());
+        }
+    }
+}
